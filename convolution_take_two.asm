bc59d38b077bde4e9912d1f674f07561304da395
mbienkow <01178511@pw.edu.pl>
2023-05-05 17:22:37 +0200

    initial commit, reading and writing from file with new register functions

---
+++ convolution_take_two.asm
@@ -0,0 +1,139 @@
+
+		.data
+
+h_buf:  	.space   54
+fname: 		.asciz  "projekt_riscv/czumpee2.bmp"
+output_name:	.asciz "projekt_riscv/convolres.bmp"
+
+#filter: 	.byte 1, 4, 6, 4, 1, 4, 16, 24, 16, 4, 6, 24, 36, 24, 6, 4, 16, 24, 16, 4, 1, 4, 6, 4, 1
+filter: 	.byte 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+#filter: 	.byte 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, -1, 4, -1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0
+#filter:		.byte 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+	   
+	
+ 	       	.text
+       	 	.globl  main
+
+main:
+
+get_file_desc:		# saves the file descriptor to a0
+	li a7, 1024
+	la a0, fname
+    	li a1, 0
+	ecall	
+		
+	mv s1, a0	# save the file descriptor
+
+read_header:		# reads the first 54 bits of the file
+	li a7, 63
+	mv a0, s1	# might delete later
+	la a1, h_buf
+	li a2, 54	# only read 54 bytes
+	ecall		# header stored in memory under h_buf
+	
+	
+store_important_header_params:	# width stored in s10, height in s11
+	la t1, h_buf		# offset of the rest of the file in s2
+				# total size in s3
+				# bytes 11-14 of the header - offset
+	lhu s2, 12(t1)		# load offset into register
+	slli s2, s2, 16		# make space for the rest of the offset
+	lh t2, 10(t1)		# second half of the offset
+	add s2, s2, t2		# add the halves together - offset in s2
+
+	lhu s3, 4(t1)		# first half of size
+	slli s3, s3, 16
+	lh t2, 2(t1)
+	add s3, s3, t2		# second half of size
+
+				# bytes 19-22 - width
+	lhu s4, 20(t1)		# first half of width
+	slli s4, s4, 16		# make place for the other half
+	lh t2, 18(t1)		# second half of width
+	add s4, s4, t2		# add the halves together
+
+				# bytes 23-26 - height
+	lhu s5, 24(t1)		# load height into register
+	slli s5, s5, 16		# make place again
+	lh t2, 22(t1)
+	add s5, s5, t2
+	
+	
+allocate_memory_for_new_file:
+	# This will be the buffer to write the convoluted pixels to
+	li a7, 9
+	mv a0, s3		# allocate memory for a whole copy of the file
+	
+	ecall
+	mv s6, a0		# address of data block in s6
+
+
+
+allocate_memory_for_pixels_only:
+	# This will be the buffer to read pixels from and convolute them
+	li a7, 9
+	mv a0, s3		# allocate memory for a whole copy of the file
+	sub a0, a0, s2		# we don't need the offset
+	
+	ecall
+	mv s7, a0		# address of data block in s7
+				
+				
+read_pixels:
+	li a7, 62
+	mv a0, s1		# seek to the start of pixel data
+	mv a1, s2		
+	mv a2, zero
+	ecall
+	
+	li a7, 63		#  read the pixel data
+	mv a0, s1
+	mv a1, s7	
+	
+	mv a2, s3
+	sub a2, a2, s2		# size - offset = size of pixel data
+	
+	ecall		
+	
+	
+write_header_and_offset:
+	li a7, 62
+	mv a0, s1		# seek to the beginning of the file
+	mv a1, zero		
+	mv a2, zero
+	ecall
+
+	li a7, 63
+	mv a0, s1		# file descriptor
+	mv a1, s6		# address of memory block
+	mv a2, s2		# file size to read - just the header + rest of the offset
+	ecall
+	
+	
+close_file:
+	li a7, 57
+	mv a0, s1
+	ecall			# close the original bmp image
+
+
+
+end:
+	jal save_file
+	li a7, 10
+	ecall
+
+	
+
+save_file:
+	li a7, 1024
+	la a0, output_name
+	li a1, 1
+	ecall
+	mv s1, a0
+	
+	li a7, 64
+	mv a0, s1
+	mv a1, s6
+	mv a2, s3
+	ecall
+	ret
